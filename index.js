a6d96139756a3e6406a393ac1ab125697130a502
Kaushik Dey <Kaushikdey647@gmail.com>
2021-12-24 01:35:50 +0530

    Updated Files

--- index.js
+++ index.js
@@ -1,3 +1,56 @@
-http = require('http')
-fs = require('fs')
-express = require('express')
+const http = require('http');
+const fs = require('fs');
+const express = require('express');
+const url = require('url');
+const slugify = require('slugify');
+
+const indexPage = fs.readFileSync(
+    `${__dirname}/pages/index-page.html`,
+    'utf-8'
+);
+
+const loginPage = fs.readFileSync(
+    `${__dirname}/pages/login-page.html`,
+    'utf-8'
+);
+
+const registerPage = fs.readFileSync(
+    `${__dirname}/pages/register-page.html`,
+    'utf-8'
+);
+
+//Server Starts Here:
+
+const server = http.createServer((req,res)=>{
+    const {query,pathname} = url.parse(req.url,true);
+    console.log(`User asked for ${pathname}`)
+    if(pathname === '/'){
+        res.writeHead(200,{
+            'Content-type': 'text/html'
+        })
+        const output = indexPage
+                    .replace('{%DIR%}',__dirname)
+                    .replace('{%TITLE%}','Home');
+        res.end(output);
+    }if(pathname === '/login'){
+        res.writeHead(200,{
+            'Content-type': 'text/html'
+        })
+        const output = loginPage
+                    .replace('{%DIR%}',__dirname)
+                    .replace('{%TITLE%}','Login');
+        res.end(output);
+    }if(pathname === '/register'){
+        res.writeHead(200,{
+            'Content-type': 'text/html'
+        })
+        const output = registerPage
+                    .replace('{%DIR%}',__dirname)
+                    .replace('{%TITLE%}','Register');
+        res.end(output);
+    }
+})
+
+server.listen(8199,'127.0.0.1',()=>{
+    console.log('Listening...')
+})
