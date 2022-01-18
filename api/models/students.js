6e4a7c25427d00bbb29eeb85a3d2d57dd77eeeea
Kaushik Dey <kaushikdey647@gmail.com>
2022-01-18 16:10:54 +0530

    Update students.js

--- api/models/students.js
+++ api/models/students.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 /*
 YOU HAVE TO GO FROM HERE
 
@@ -96,3 +98,169 @@ TO HERE
 
 
 */
+
+//schema for institute details
+const insti = new mongoose.Schema({
+  programme: {
+    type: String,
+  },
+  institute: {
+    type: String,
+  },
+  specialization: {
+    type: String,
+  },
+  year: {
+    type: Number,
+  },
+  division: {
+    type: Number,
+  },
+  percentage: {
+    type: Number,
+  },
+  cgpa: {
+    type: Number,
+  },
+});
+
+const net = new mongoose.Schema({
+  discipline: {
+    type: String,
+  },
+  year: {
+    type: Number,
+  },
+})
+
+const creds = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+})
+
+const gate = new mongoose.Schema({
+  discipline: {},
+  score: {},
+  percentile: {},
+  rank: {},
+  year: {},
+  validity: {},
+})
+
+const bio = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A Name Is Required"],
+  },
+  type: {
+    type: String,
+    required: [true, "A Name Is Required"],
+  },
+  sponsoredAgency: {
+    type: String,
+  },
+  school: {
+    type: String,
+    required: [true, "Please Specify School"],
+  },
+  discipline: {
+    type: String,
+    required: [true, "Please Specify Discipline"],
+  },
+  researchArea1: {
+    type: String,
+    required: [true, "A Name Is Required"],
+  },
+  gender: {
+    type: String,
+    required: [true, "A Name Is Required"],
+  },
+  category: {
+    type: String,
+    required: [true, "A Name Is Required"],
+  },
+  pwd: {
+    type: Boolean,
+    required: [true, "A Name Is Required"],
+  },
+  mtechthroughgate: {
+    type: Boolean,
+    required: [true, "A Name Is Required"],
+  },
+})
+
+const experience = new mongoose.Schema({
+  employer: {
+
+  },
+  designation: {
+
+  },
+  periodbegin: {
+    type: Date
+  },
+  periodmonths: {
+
+  },
+  nature: {
+
+  },
+})
+
+const studentSchema = new mongoose.Schema({
+  credentials: {
+    type: creds,
+    required: true,
+  },
+  bio: {
+    type: bio,
+    required: true,
+    
+  },
+  tenth: {
+    type: insti,
+    required: true,
+    
+  },
+  twelfth: {
+    type: insti,
+    required: true,
+    
+  },
+  bachelors: {
+    type: insti,
+    required: true,
+    
+  },
+  masters: {
+    type: insti,
+    
+  },
+  gate: {
+    type: gate,
+    
+  },
+  net: {
+    type: net,
+    
+  },
+  others: {
+    type: [insti],
+    
+  },
+  experience: {
+    type: [experience],
+    
+  },
+});
+
+const Student = mongoose.model('Student', studentSchema); //'Student' is the collection the model is for, and StudentSchema is well, the schema
+//now Student is the object that can be used to fill in new objects
+module.exports = Student;
